e47d2f8ccc0f5ca0f01bdbfab6061ea87543fca4
Hugo <114680022+hugorodriv@users.noreply.github.com>
2025-02-03 13:37:17 +0100

    add redacted secrets.py reference

---
+++ secrets.py
@@ -0,0 +1,8 @@
+# IP Webcam
+USERNAME = "username"                           # Username for IP Webcam HTTPS auth
+PASSWORD = "password"                           # Username for IP Webcam HTTPS auth
+ENDPOINT = "https://<phone_ip>:8080/shot.jpg"   # Endoiunt for IP Webcam image. This is the default
+
+# Telegram
+TELEGRAM_TOKEN = "123456789:AAAAABBBBBBCCCCC"   # Telegram bot token XXXXX:XXXXXXXXXXXX
+TELEGRAM_GROUP_ID = "(-100)00000000000"         # Telegram group (or chat) ID. If it's a group ID, append '-100' to the code
